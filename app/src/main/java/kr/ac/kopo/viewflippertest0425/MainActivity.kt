cf8e6e5892a3b9e641d2b542e87bfaea1e7443b7
rrvv123 <eorms7391@gmail.com>
2024-05-09 14:06:51 +0900

    0425 추가 작성 + 0509 ViewFlipper 예제 1

--- app/src/main/java/kr/ac/kopo/viewflippertest0425/MainActivity.kt
+++ app/src/main/java/kr/ac/kopo/viewflippertest0425/MainActivity.kt
@@ -1,20 +1,36 @@
 package kr.ac.kopo.viewflippertest0425
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.ViewFlipper
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    lateinit var btnNext : Button
+    lateinit var btnPrev : Button
+    lateinit var vFlip : ViewFlipper
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        btnNext = findViewById<Button>(R.id.btnNext)
+        btnPrev = findViewById<Button>(R.id.btnPrev)
+        vFlip = findViewById<ViewFlipper>(R.id.flipper)
+
+        btnNext.setOnClickListener(btnListener)
+        btnPrev.setOnClickListener(btnListener)
+
+    }
+
+    val btnListener = View.OnClickListener {
+        when(it.id){
+            R.id.btnNext -> vFlip.showNext()
+            R.id.btnPrev -> vFlip.showPrevious()
         }
     }
 }
