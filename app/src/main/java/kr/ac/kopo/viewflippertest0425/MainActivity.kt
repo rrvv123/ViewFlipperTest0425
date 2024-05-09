abe62bdd2a94d9de7effe60c14a89007e160ce70
rrvv123 <eorms7391@gmail.com>
2024-05-09 14:24:51 +0900

    0509 직접풀어보기 6-2 7교시중

--- app/src/main/java/kr/ac/kopo/viewflippertest0425/MainActivity.kt
+++ app/src/main/java/kr/ac/kopo/viewflippertest0425/MainActivity.kt
@@ -10,27 +10,29 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
-    lateinit var btnNext : Button
-    lateinit var btnPrev : Button
+    lateinit var btnStart : Button
+    lateinit var btnStop : Button
     lateinit var vFlip : ViewFlipper
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        btnNext = findViewById<Button>(R.id.btnNext)
-        btnPrev = findViewById<Button>(R.id.btnPrev)
+        btnStart = findViewById<Button>(R.id.btnStart)
+        btnStop = findViewById<Button>(R.id.btnStop)
         vFlip = findViewById<ViewFlipper>(R.id.flipper)
 
-        btnNext.setOnClickListener(btnListener)
-        btnPrev.setOnClickListener(btnListener)
+        btnStart.setOnClickListener(btnListener)
+        btnStop.setOnClickListener(btnListener)
+
+        vFlip.setFlipInterval(2000)
 
     }
 
     val btnListener = View.OnClickListener {
         when(it.id){
-            R.id.btnNext -> vFlip.showNext()
-            R.id.btnPrev -> vFlip.showPrevious()
+            R.id.btnStart -> vFlip.startFlipping()
+            R.id.btnStop -> vFlip.stopFlipping()
         }
     }
 }
